c1e4be3761d3bf5798768b60ecbf0e7bebc0e8fc
Choicky ZHOU <zhoucaiqi@gmail.com>
2015-02-07 02:36:27 +0800

    listClient Chinese Character ordering
    listClient Chinese Character ordering

--- Application/Home/Controller/ClientController.class.php
+++ Application/Home/Controller/ClientController.class.php
@@ -9,7 +9,7 @@ class ClientController extends Controller {
 	
 	public function listClient(){
 		$client = D('Client');
-		$clientList = $client->relation(true)->order('name_zh')->select();
+		$clientList = $client->relation(true)->order('convert(name_zh using gb2312) asc')->select();
 		//var_dump($clientList);
 		$this->assign('clientList',$clientList);
 		$this->display();
