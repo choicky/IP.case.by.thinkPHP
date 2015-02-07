40f8b7a769d99df6396aafe341629b46e279242f
Choicky ZHOU <zhoucaiqi@gmail.com>
2015-02-07 21:21:00 +0800

    revise Page Class again
    revise Page Class again

--- ThinkPHP/Library/Think/Page.class.php
+++ ThinkPHP/Library/Think/Page.class.php
@@ -26,8 +26,8 @@ class Page{
 	// 分页显示定制
     private $config  = array(
         'header' => '<span class="rows">共 %TOTAL_ROW% 条记录</span>',
-        'prev'   => '<<',
-        'next'   => '>>',
+        'prev'   => '上一页',
+        'next'   => '下一页',
         'first'  => '1...',
         'last'   => '...%TOTAL_PAGE%',
         'theme'  => '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%',
