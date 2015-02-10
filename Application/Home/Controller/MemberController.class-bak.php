3d6a1c27df6c8f7a7d48f114a854240b947f91d1
Choicky ZHOU <zhoucaiqi@gmail.com>
2015-02-10 23:51:39 +0800

    Member MVC improved
    Member MVC improved

--- Application/Home/Controller/MemberController.class-bak.php
+++ Application/Home/Controller/MemberController.class-bak.php
@@ -3,24 +3,19 @@ namespace Home\Controller;
 use Think\Controller;
 
 class MemberController extends Controller {
-    
-	//默认跳转到listMember，显示member表列表
-	public function index(){
+    public function index(){
         header("Location: listMember");
     }
 	
-	//列表，其中，$p为当前分页数，$limit为每页显示的记录数
 	public function listMember(){
 		$p	= I("p",1,"int");
-		$limit	= 10;
-		$member_list = D('Member')->listMember($p,$limit);
-		$this->assign('member_list',$member_list['member_list']);
-		$this->assign('member_page',$member_list['page']);
-
+		$limit	= 10;		
+		$list = D('Member')->listMember($p,$limit);
+		//var_dump($list);
+		$this->assign('list',$list);
 		$this->display();
 	}
 	
-	//新增
 	public function add(){
 		$data	=	array();
 		$data['member_name']	=	trim(I('post.member_name'));
@@ -28,9 +23,9 @@ class MemberController extends Controller {
 		$data['member_phone']	=	trim(I('post.member_phone'));
 		
 		if(!$data['member_name']){
-			$this->error('未填写姓名');
+			$this->error('未填写成员姓名');
 		} 
-
+				
 		$result = D('Member')->addMember($data);
 		
 		if(false !== $result){
@@ -49,25 +44,25 @@ class MemberController extends Controller {
 			$data['member_name']	=	trim(I('post.member_name'));
 			$data['member_email']	=	trim(I('post.member_email'));
 			$data['member_phone']	=	trim(I('post.member_phone'));
-
+						
+			//var_dump($data);
 			$result = D('Member')->editMember($member_id,$data);
 			if(false !== $result){
 				$this->success('修改成功', 'listMember');
-				//header("Location: listMember");
+				//header("Location: listCaseType");
 			}else{
-				$this->error('修改失败');
+				$this->error('修改成功');
 			}
 		} else{
 			$member_id = I('get.id',0,'int');
 
 			if(!$member_id){
-				$this->error('未指明要编辑的成员');
+				$this->error('未指明要编辑的客户');
 			}
 
-			$member_list = D('Member')->getByMemberId($member_id);
+			$member_info = D('Member')->getByMemberId($member_id);
 			
-			$this->assign('member_list',$member_list);
-
+			$this->assign('member_info',$member_info);
 			$this->display();
 		}
 	}
