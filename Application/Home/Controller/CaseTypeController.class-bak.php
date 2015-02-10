638777ac00b11f57f59af181827066e4d5cd7153
Choicky ZHOU <zhoucaiqi@gmail.com>
2015-02-11 01:05:45 +0800

    CaseType MVC improved
    CaseType MVC improved

--- Application/Home/Controller/CaseTypeController.class-bak.php
+++ Application/Home/Controller/CaseTypeController.class-bak.php
@@ -3,32 +3,27 @@ namespace Home\Controller;
 use Think\Controller;
 
 class CaseTypeController extends Controller {
-    
-	//默认跳转到listCaseType，显示case_type表列表
-	public function index(){
+    public function index(){
         header("Location: listCaseType");
     }
 	
-	//列表，其中，$p为当前分页数，$limit为每页显示的记录数
 	public function listCaseType(){
 		$p	= I("p",1,"int");
-		$limit	= 10;
-		$case_type_list = D('CaseType')->listCaseType($p,$limit);
-		$this->assign('case_type_list',$case_type_list['case_type_list']);
-		$this->assign('case_type_page',$case_type_list['case_type_page']);
-
+		$limit	= 10;		
+		$list = D('CaseType')->listCaseType($p,$limit);
+		//var_dump($list);
+		$this->assign('list',$list);
 		$this->display();
 	}
 	
-	//新增
 	public function add(){
 		$data	=	array();
 		$data['case_type_name']	=	trim(I('post.case_type_name'));
 		
 		if(!$data['case_type_name']){
-			$this->error('未填写费用名称');
+			$this->error('未填写案件类型名称');
 		} 
-
+		
 		$result = D('CaseType')->addCaseType($data);
 		
 		if(false !== $result){
@@ -37,15 +32,18 @@ class CaseTypeController extends Controller {
 			$this->error('增加失败');
 		}
 	}
-		
+	
+	
+	
 	public function edit(){
 		if(IS_POST){
 			
 			$case_type_id	=	trim(I('post.case_type_id'));
 			
 			$data=array();
-			$data['case_type_name']	=	trim(I('post.case_type_name'));
-
+			$data['case_type_name'] = trim(I('case_type_name'));
+						
+			//var_dump($data);
 			$result = D('CaseType')->editCaseType($case_type_id,$data);
 			if(false !== $result){
 				$this->success('修改成功', 'listCaseType');
@@ -57,13 +55,12 @@ class CaseTypeController extends Controller {
 			$case_type_id = I('get.id',0,'int');
 
 			if(!$case_type_id){
-				$this->error('未指明要编辑的客户');
+				$this->error('未指明要编辑的费用');
 			}
 
-			$case_type_list = D('CaseType')->getByCaseTypeId($case_type_id);
+			$case_info = D('CaseType')->getByCaseTypeId($case_type_id);
 			
-			$this->assign('case_type_list',$case_type_list);
-
+			$this->assign('case_info',$case_info);
 			$this->display();
 		}
 	}
--- Application/Home/Model/CaseTypeModel.class.php
+++ Application/Home/Model/CaseTypeModel.class.php
@@ -1,29 +1,32 @@
 <?php
 namespace Home\Model;
-use Think\Model;
-use Think\Model\RelationModel;
 
-class CaseTypeModel extends RelationModel {
+use Think\Model;
+//因为没有数据表关联，注释RelationModel
+//use Think\Model\RelationModel;
+
+class CaseTypeModel extends Model {
+	
+	//获取case_type表的列表，$p为当前页数，$limit为每页显示的记录条数
 	public function listCaseType($p,$limit) {
-		//$p为当前页数，$limit为每页显示的记录条数
-		$data	= $this->order('convert(case_type_name using gb2312) asc')->page($p.','.$limit)->select();
+		$case_type_list	= $this->order('convert(case_type_name using gb2312) asc')->page($p.','.$limit)->select();
 		
-		$count	= $this->count();
+		$case_type_count	= $this->count();
 		
-		$Page	= new \Think\Page($count,$limit);
+		$Page	= new \Think\Page($case_type_count,$limit);
 		$show	= $Page->show();
 		
-		return array("list"=>$data,"page"=>$show);
+		return array("case_type_list"=>$case_type_list,"case_type_page"=>$show);
 	}
 	
+	//向case_type表插入记录，$data是数组，且不包含主键
 	public function addCaseType($data){
-		//$data是数组，且不包含主键
 		$result	=	$this->add($data);
 		return $result;
 	}
 	
+	//更新case_type表中主键为$case_type_id的记录，$data是数组
 	public function editCaseType($case_type_id,$data){
-		//$case_type_id为主键，$data是数组，且不包含主键
 		$map['case_type_id']	=	$case_type_id;
 		$result	=	$this->where($map)->save($data);
 		return $result;
