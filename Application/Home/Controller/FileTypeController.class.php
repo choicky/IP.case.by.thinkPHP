c4edca3bfd79453f6a1db9394346c328c618659d
Choicky ZHOU <zhoucaiqi@gmail.com>
2015-02-11 00:59:15 +0800

    FileType MVC improved
    FileType MVC improved

--- Application/Home/Controller/FileTypeController.class.php
+++ Application/Home/Controller/FileTypeController.class.php
@@ -3,27 +3,32 @@ namespace Home\Controller;
 use Think\Controller;
 
 class FileTypeController extends Controller {
-    public function index(){
+    
+	//默认跳转到listFileType，显示file_type表列表
+	public function index(){
         header("Location: listFileType");
     }
 	
+	//列表，其中，$p为当前分页数，$limit为每页显示的记录数
 	public function listFileType(){
 		$p	= I("p",1,"int");
-		$limit	= 10;		
-		$list = D('FileType')->listFileType($p,$limit);
-		//var_dump($list);
-		$this->assign('list',$list);
+		$limit	= 10;
+		$file_type_list = D('FileType')->listFileType($p,$limit);
+		$this->assign('file_type_list',$file_type_list['file_type_list']);
+		$this->assign('file_type_page',$file_type_list['file_type_page']);
+
 		$this->display();
 	}
 	
+	//新增
 	public function add(){
 		$data	=	array();
-		$data['file_type_name']	=	trim(I('post.file_type_name'));
+		$data['file_name']	=	trim(I('post.file_name'));
 		
-		if(!$data['file_type_name']){
-			$this->error('未填写案件类型名称');
+		if(!$data['file_name']){
+			$this->error('未填写费用名称');
 		} 
-		
+
 		$result = D('FileType')->addFileType($data);
 		
 		if(false !== $result){
@@ -32,18 +37,15 @@ class FileTypeController extends Controller {
 			$this->error('增加失败');
 		}
 	}
-	
-	
-	
+		
 	public function edit(){
 		if(IS_POST){
 			
 			$file_type_id	=	trim(I('post.file_type_id'));
 			
 			$data=array();
-			$data['file_type_name'] = trim(I('file_type_name'));
-						
-			//var_dump($data);
+			$data['file_name']	=	trim(I('post.file_name'));
+
 			$result = D('FileType')->editFileType($file_type_id,$data);
 			if(false !== $result){
 				$this->success('修改成功', 'listFileType');
@@ -55,12 +57,13 @@ class FileTypeController extends Controller {
 			$file_type_id = I('get.id',0,'int');
 
 			if(!$file_type_id){
-				$this->error('未指明要编辑的费用');
+				$this->error('未指明要编辑的客户');
 			}
 
-			$file_info = D('FileType')->getByCaseTypeId($file_type_id);
+			$file_type_list = D('FileType')->getByFileTypeId($file_type_id);
 			
-			$this->assign('file_info',$file_info);
+			$this->assign('file_type_list',$file_type_list);
+
 			$this->display();
 		}
 	}
--- Application/Home/Model/FileTypeModel.class.php
+++ Application/Home/Model/FileTypeModel.class.php
@@ -1,29 +1,32 @@
 <?php
 namespace Home\Model;
-use Think\Model;
-use Think\Model\RelationModel;
 
-class FileTypeModel extends RelationModel {
+use Think\Model;
+//因为没有数据表关联，注释RelationModel
+//use Think\Model\RelationModel;
+
+class FileTypeModel extends Model {
+	
+	//获取file_type表的列表，$p为当前页数，$limit为每页显示的记录条数
 	public function listFileType($p,$limit) {
-		//$p为当前页数，$limit为每页显示的记录条数
-		$data	= $this->order('convert(file_type_name using gb2312) asc')->page($p.','.$limit)->select();
+		$file_type_list	= $this->order('convert(file_name using gb2312) asc')->page($p.','.$limit)->select();
 		
-		$count	= $this->count();
+		$file_type_count	= $this->count();
 		
-		$Page	= new \Think\Page($count,$limit);
+		$Page	= new \Think\Page($file_type_count,$limit);
 		$show	= $Page->show();
 		
-		return array("list"=>$data,"page"=>$show);
+		return array("file_type_list"=>$file_type_list,"file_type_page"=>$show);
 	}
 	
+	//向file_type表插入记录，$data是数组，且不包含主键
 	public function addFileType($data){
-		//$data是数组，且不包含主键
 		$result	=	$this->add($data);
 		return $result;
 	}
 	
+	//更新file_type表中主键为$file_type_id的记录，$data是数组
 	public function editFileType($file_type_id,$data){
-		//$file_type_id为主键，$data是数组，且不包含主键
 		$map['file_type_id']	=	$file_type_id;
 		$result	=	$this->where($map)->save($data);
 		return $result;
