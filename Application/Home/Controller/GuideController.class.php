42a007684bb38ec3cc140c5d71225a9e2be69b8e
Choicky ZHOU <zhoucaiqi@gmail.com>
2015-04-15 00:30:19 +0800

    修改菜单

---
+++ Application/Home/Controller/GuideController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class GuideController extends Controller {
+
+	public function caseGuide(){
+		$this->display();
+    }
+	
+	public function feeGuide(){
+		$this->display();
+    }
+}
