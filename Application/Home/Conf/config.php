e5741d55b164930d2b51a943d6c2661f94ac5822
Choicky ZHOU <zhoucaiqi@gmail.com>
2016-12-05 14:44:19 +0800

    add nginx config

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -11,7 +11,7 @@ return array(
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'thinkPHP',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
+    'DB_PWD'                =>  'password',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'tp_',    // 数据库表前缀
 	
