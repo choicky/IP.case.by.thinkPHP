313ac534a3f012ae8d987a0bf98dbebb6b372596
Choicky ZHOU <zhoucaiqi@gmail.com>
2017-05-21 14:18:20 +0800

    增加两个 测试模板
    testV3Dashboard 和  testV4Dashboard

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -5,22 +5,29 @@ use Think\Controller;
 class TestController extends Controller {
 
 	public function index(){
-		$case_list_tmp	=	M('Case')->field('case_id,our_ref')->select();
-		
-		for($j=0;$j<count($case_list_tmp);$j++){
-			$case_id	=	$case_list_tmp[$j]['case_id'];
-			$our_ref	=	$case_list_tmp[$j]['our_ref'];
-			
-			$map_case_fee['case_id']	=	$case_id;
-			$map_case_fee['case_payment_id']	=	array('GT',2);
-			$case_fee_list	=	M('CaseFee')->where($map_case_fee)->count();
-			
-			if($case_fee_list>2){
-				$this->show('<a href="'.U('Case/view','case_id='.$case_id).'">'.$our_ref.'</a><br>');
-			}
-			
-		}
-	
-    }
-	
+    
+    //返回当前时间
+    $title = "测试页面";
+    $this->assign('title',$title);
+      
+		$this->display();
+  }
+  
+  public function testV3Dashboard(){
+    
+    //返回当前时间
+    $title = "测试页面";
+    $this->assign('title',$title);
+      
+		$this->display();
+  }
+  
+  public function testV4(){
+    
+    //返回当前时间
+    $title = "测试页面";
+    $this->assign('title',$title);
+      
+		$this->display();
+  }
 }
