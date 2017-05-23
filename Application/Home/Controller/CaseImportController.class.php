ad773bb9a99e63b0f9674cf81b438712e3c3ef54
Choicky ZHOU <zhoucaiqi@gmail.com>
2017-05-23 23:00:20 +0800

    improve caseImport controller

--- Application/Home/Controller/CaseImportController.class.php
+++ Application/Home/Controller/CaseImportController.class.php
@@ -15,169 +15,187 @@ class CaseImportController extends Controller {
 	//导入
 	public function import(){
 		
-    //读取原表的数据
-    $case_source_list  = M('CaseSource')->select();
-    $case_source_count  = count($case_source_list);
-    
-    
-    for($i=0,$n=0;$i<$case_source_count;$i++,$n++){
-           
-      //构造maping
-      $map_case_for_find['application_number']  = trim($case_source_list[$i]['application_number']);
-      
-      //读取Case表，并计数
-      $case_list  = M('Case')->where($map_case_for_find)->select();
-      $case_count  = count($case_list);
-      
-      //如果没有匹配，则原数据存入到 CaseOutput 表；否则进行处理
-      if(!$case_count){
-        $case_source_list[$i]['notfound'] = '白兔系统找到这个案子，但盈方管理系统没有找到；如该商标是有效的，要录入 admin.ipinfo.cn ';
-        dump($case_source_list[$i]);
-       $result	=	M('CaseOutput')->add($case_source_list[$i]);
+        //读取 case_source 表
+        $case_source_list  = M( 'CaseSource')->select();
+        
+        /* 
+        $i 为case_source 表的记录数量
+        $number_of_new_case 为新case数量，
+        $number_of_update_case 为更新的case数量，
+        $number_of_diff_case 为信息有差异的case数量，
+        $number_of_add_extend 为新增到 case_extend 的数量，
+        $number_of_update_extend 为修改 case_extend 的数量         */        
+        for( $i=0,$number_of_new_case=0,$number_of_update_case=0,$number_of_diff_case=0,$number_of_add_extend,$number_of_update_extend; $i<count( $case_source_list); $i++){
+                   
+            //用 application_number 作为匹配关键词
+            $map_case_for_find['application_number']  = trim( $case_source_list[$i]['application_number'] );
+            
+            //找出 case 表中的匹配记录
+            $case_list  = M( 'Case' )->where( $map_case_for_find)->select();
+            
+            if(!is_array($case_list)){ //如果 case 表没有匹配的记录，则原数据存入到 CaseOutput 表
+                $case_source_list[$i]['notfound'] = '白兔系统找到这个案子，但盈方管理系统没有找到；如该商标是有效的，要录入 admin.ipinfo.cn ';
+                $case_source_list[$i]['application_date'] = strtotime(trim( $case_source_list[$i]['application_date']));
+                $case_source_list[$i]['issue_date'] = strtotime(trim( $case_source_list[$i]['issue_date']));
 
-       if(false !== $result){
-          //$this->success('修改成功', U('Case/view','case_id='.$case_id));
-        }else{
-          $this->error('notfound的案件保存失败'.$i);
-        }
-        
-      }else{
-        
-        // n 用来统计存入到 CaseOutput 表 的数量
-        $n  = $n  + $case_count -1;
-        
-        for($j=0;$j<$case_count;$j++){
-          
-          //先更新 Case_extend
-          $map_case_extend_for_find['case_id'] = $case_list[$j]['case_id']; 
-          $case_extend_list = M('CaseExtend')->where($map_case_extend_for_find)->find();
-          $case_extend_count  = count($case_extend_list);
-          
-          if($case_extend_count){
-            
-            $map_case_extend_for_update['case_extend_id'] = $case_extend_list['case_extend_id'];
-            $case_extend_data['remarks'] = $case_extend_list['remarks'].'+'.$case_source_list[$i]['remarks'];
-            $result	=	M('CaseExtend')->where($map_case_extend_for_update)->save($case_extend_data);
-            
-            if(false !== $result){
-              //$this->success('修改成功', U('Case/view','case_id='.$case_id));
-            }else{
-              $this->error('更新 Extend 失败'.$i.'和'.$j);
-            }
-            
-          }else{
-            
-            $case_extend_data['remarks'] = $case_source_list[$i]['remarks'];
-            $case_extend_data['case_id'] = $case_list[$j]['case_id'];
-            $result	=	M('CaseExtend')->add($case_extend_data);
-            
-            if(false !== $result){
-              //$this->success('修改成功', U('Case/view','case_id='.$case_id));
-            }else{
-              $this->error('新增 Extend 失败'.$i.'和'.$j);
-            }
-          }
-          
-          //下面更新主表
-          
-          
-          //判断分类号是否相同
-          //先判断 case_source 是否有数据
-          if(!trim($case_source_list[$i]['formal_title'])){
-            $case_source_list[$i]['formal_title'] = $case_list[$j]['formal_title'];
-          }else{            
-            //如 case 有数据，才比较差异
-            if(!trim($case_list[$j]['formal_title'])){
-              $formal_title_test  = trim($case_source_list[$i]['formal_title'])  - trim($case_list[$j]['formal_title']);
-              if($formal_title_test){
-                $case_source_list[$i]['formal_title_test'] = '盈方系统原来登记的分类号是：'.trim($case_list[$j]['formal_title']).'，'.'已将白兔的分类号：'.$case_source_list[$i]['formal_title'].'，加到中括号里';
-              }
-            }
-            
-            //将 case_source 的数据附加到 case 的数据后面
-            $case_source_list[$i]['formal_title'] = trim($case_list[$j]['formal_title']).'['.$case_source_list[$i]['formal_title'].']';            
-          }
-          
-          //判断商标名称是否相同
-          //先判断 case_source 是否有数据
-          if(!trim($case_source_list[$i]['tentative_title'])){
-            $case_source_list[$i]['tentative_title'] = $case_list[$j]['tentative_title'];
-          }else{            
-            //如 case 有数据，才比较差异
-            if(trim($case_list[$j]['tentative_title'])){
-              $tentative_title_test  = strcmp(trim($case_source_list[$i]['tentative_title']),trim($case_list[$j]['tentative_title']));
-              if($tentative_title_test){
-                $case_source_list[$i]['tentative_title_test'] = '盈方系统原来登记的商标名称是：'.trim($case_list[$j]['tentative_title']).'，'.'已将白兔的商标名称：'.$case_source_list[$i]['tentative_title'].'加到中括号里';
-              }
-            }
-            
-            //将 case_source 的数据附加到 case 的数据后面
-            $case_source_list[$i]['tentative_title'] = trim($case_list[$j]['tentative_title']).'['.$case_source_list[$i]['tentative_title'].']';            
-          }
-          
-          //判断申请日是否相同
-          //先判断 case_source 是否有数据
-          if(!trim($case_source_list[$i]['application_date'])){
-            $case_source_list[$i]['application_date'] = $case_list[$j]['application_date'];
-          }else{            
-            //如 case 有数据，才比较差异
-            if(trim($case_list[$j]['application_date'])){
-              $application_date_test  = strtotime(trim($case_source_list[$i]['application_date']))  - trim($case_list[$j]['application_date']);
-              if($application_date_test){
-                $case_source_list[$i]['application_date_test'] = '盈方系统原来登记的申请日是：'.date("Y-m-d",trim($case_list[$j]['application_date'])).'，'.'已更新为白兔的申请日：'.trim($case_source_list[$i]['application_date']).'，请核对';
-              }
-            }
-            //将 case_source 的数据变换格式
-            $case_source_list[$i]['application_date'] = strtotime(trim($case_source_list[$i]['application_date']));            
-          }
-          
-          //判断发证日是否相同
-          //先判断 case_source 是否有数据
-          if(!trim($case_source_list[$i]['issue_date'])){
-            $case_source_list[$i]['issue_date'] = $case_list[$j]['issue_date'];
-          }else{
-            //如 case 有数据，才比较差异
-            if(trim($case_list[$j]['issue_date'])){
-              $issue_date_test  = strtotime(trim($case_source_list[$i]['issue_date']))  - trim($case_list[$j]['issue_date']);
-              if($issue_date_test){
-                $case_source_list[$i]['issue_date_test'] = '盈方系统原来登记的发证日是：'.date("Y-m-d",trim($case_list[$j]['issue_date'])).'，'.'已更新为白兔的发证日：'.trim($case_source_list[$i]['issue_date']).'，请核对';
-              }
-            }
-            //将 case_source 的数据变换格式
-            $case_source_list[$i]['issue_date'] = strtotime(trim($case_source_list[$i]['issue_date']));            
-          }
-          
-          $map_case_for_update['case_id']   =  $case_list[$j]['case_id'];
-          $result	=	M('Case')->where($map_case_for_update)->field('formal_title,tentative_title,application_date,issue_date')->save($case_source_list[$i]);
-          
-          if(false !== $result){
-            //$this->success('修改成功', U('Case/view','case_id='.$case_id));
-          }else{
-            $this->error('修改 Case 失败'.$i.'和'.$j);
-          }
-          
-          if($formal_title_test OR $tentative_title_test OR $application_date_test OR $issue_date_test){
-            $case_source_list[$i]['case_id'] = $case_list[$j]['case_id'];
-            dump($case_source_list[$i]);
-            $result	=	M('CaseOutput')->add($case_source_list[$i]);
-          }       
-          
-          if(false !== $result){
-            //$this->success('修改成功', U('Case/view','case_id='.$case_id));
-          }else{
-            $this->error('校验后新增 CaseExtend 失败'.$i.'和'.$j);
-          }
+                $result	=	M('CaseOutput')->field('tentative_title,application_number,application_date,formal_title,issue_date,remarks')->add($case_source_list[$i]);
 
-          
+                if(false !== $result){
+                    // $number_of_new_case 为新case数量
+                    $number_of_new_case  = $number_of_new_case  + 1;
+                    echo('case_source表第'.$i.'case表第'.$j.',新增到 case_output<br>');
+                    dump($case_source_list[$i]);
+                    
+                }else{
+                    $this->error('notfound的案件保存失败'.$i);
+                }
+                
+            }else{  //如果 case 表有匹配的记录，则更新
+                 
+                // $j 为 case 匹配的记录数量，先更新主表
+                for($j=0;$j<count($case_list);$j++){                
+
+                    $case_id = $case_list[$j]['case_id'];
+                    $case_data[$case_id] = $case_list[$j];
+                    
+                    //判断分类号是否相同
+                    if(trim( $case_source_list[$i]['formal_title'])){  //如果 case_source 有相应数据
+                        if(trim( $case_data[$case_id]['formal_title'])){ //如果 case 有相应数据
+                            $formal_title_test  = trim( $case_source_list[$i]['formal_title'])  - trim( $case_data[$case_id]['formal_title']);
+                            if( $formal_title_test){    //如果数据不相同
+                                $case_data[$case_id]['formal_title_test'] = '盈方系统原来登记的分类号是：'.trim($case_data[$case_id]['formal_title']).'，'.'已将白兔的分类号：'.$case_source_list[$i]['formal_title'].'，加到中括号里';
+                                $case_data[$case_id]['formal_title'] = trim( $case_data[$case_id]['formal_title']).'['.trim( $case_source_list[$i]['formal_title']).']';
+                            }
+                        }
+                    }
+                      
+                    //判断商标名称是否相同
+                    if(trim( $case_source_list[$i]['tentative_title'])){   //如果 case_source 有相应数据
+                        if(trim( $case_data[$case_id]['tentative_title'])){   //如果 case 有相应数据
+                            $tentative_title_test  = strpos( trim( $case_data[$case_id]['tentative_title']),trim( $case_source_list[$i]['tentative_title']));
+                            if( FALSE == $tentative_title_test ){   //如果 Case 的数据未包含 Case_Source 的数据
+                                $case_data[$case_id]['tentative_title_test'] = '盈方系统原来登记的商标名称是：'.trim( $case_data[$case_id]['tentative_title']).'，'.'已将白兔的商标名称：'.trim( $case_source_list[$i]['tentative_title']).'加到中括号里';
+                                $case_data[$case_id]['tentative_title'] = trim( $case_data[$case_id]['tentative_title']).'['.trim( $case_source_list[$i]['tentative_title']).']'; 
+                            }
+                        }
+                    }
+                      
+                    //判断申请日是否相同
+                    if(trim( $case_source_list[$i]['application_date'])){  //如果 case_source 有相应数据
+                        if(trim( $case_data[$case_id]['application_date'])){  //如果 case 有相应数据
+                            $application_date_test  = strtotime(trim( $case_source_list[$i]['application_date']))  - trim( $case_data[$case_id]['application_date']);
+                            if( $application_date_test){    //如果数据不相同
+                                $case_data[$case_id]['application_date_test'] = '盈方系统原来登记的申请日是：'.date("Y-m-d",trim( $case_data[$case_id]['application_date'])).'，'.'已更新为白兔的申请日：'.trim( $case_source_list[$i]['application_date']).'，请核对';
+                                $case_data[$case_id]['application_date'] = strtotime(trim( $case_source_list[$i]['application_date'])); 
+                            }
+                        }
+                    }
+                      
+                    //判断发证日是否相同
+                    if(trim( $case_source_list[$i]['issue_date'])){    //如果 case_source 有相应数据
+                        if(trim( $case_data[$case_id]['issue_date'])){    //如果 case 有相应数据
+                            $issue_date_test  = strtotime(trim( $case_source_list[$i]['issue_date']))  - trim( $case_data[$case_id]['issue_date']);
+                            if( $issue_date_test){  //如果数据不相同
+                                $case_data[$case_id]['issue_date_test'] = '盈方系统原来登记的发证日是：'.date("Y-m-d",trim( $case_data[$case_id]['issue_date'])).'，'.'已更新为白兔的发证日：'.trim( $case_source_list[$i]['issue_date']).'，请核对';
+                                $case_data[$case_id]['issue_date'] = strtotime(trim( $case_source_list[$i]['issue_date']));
+                            }
+                        }
+                    }
+                      
+                    $map_case_for_update[$case_id]['case_id']   =  $case_id;
+                    
+                    $result	=	M('Case')->where( $map_case_for_update[$case_id])->field('tentative_title,application_number,application_date,formal_title,issue_date')->save( $case_data[$case_id]);
+                    //$result	=	M('Case')->field('case_id,tentative_title,application_number,application_date,formal_title,issue_date')->save( $case_data[$case_id]);
+                    
+                    if(false !== $result){
+                        // $number_of_update_case 用来统计有差异的case数量
+                        $number_of_update_case  = $number_of_update_case  + 1;
+                        echo('case_source表第'.$i.'case表第'.$j.',更新到 case<br>');
+                        dump($case_data[$case_id]);
+                        //$this->success('修改成功', U('Case/view','case_id='.$case_id));
+                    }else{
+                        $this->error('修改 Case 失败'.$i.'和'.$j);
+                    }
+                    
+                    // 将有差异的记录存储到 case_output
+                    if($formal_title_test OR (FALSE == $tentative_title_test) OR $application_date_test OR $issue_date_test){
+                
+                        $result	=	M('CaseOutput')->field('case_id,tentative_title,tentative_title_test,application_number,application_number_test,application_date,application_date_test,formal_title,formal_title_test,issue_date,issue_date_test')->add( $case_data[$case_id]);
+
+                    } 
+                      
+                    if(false !== $result){
+                        // $number_of_diff_case 用来统计有差异的case数量
+                        $number_of_diff_case  = $number_of_diff_case  + 1;
+                        echo('case_source表第'.$i.'case表第'.$j.',新增到 case_output<br>');
+                        dump($case_source_list[$i]);
+                        //$this->success('修改成功', U('Case/view','case_id='.$case_id));
+                    }else{
+                        $this->error('校验后新增 CaseExtend 失败'.$i.'和'.$j);
+                    }
+                    
+
+                    // 下面更新麻烦的 case_extend 表
+                    if(trim( $case_source_list[$i]['remarks'])){   //如果 case_source 有 remarks
+                        
+                        $map_case_extend_for_find['case_id'] = $case_list[$j]['case_id'];
+                        $case_extend_list = M('CaseExtend')->where( $map_case_extend_for_find)->find();
+                        
+                        if(!is_array($case_extend_list)){   // case_extend 表没有对应于 case_id 的记录
+                            $case_extend_data[$case_id]['case_id'] = $case_list[$j]['case_id'];
+                            $case_extend_data[$case_id]['remarks'] = trim( $case_source_list[$i]['remarks']);
+                            $result	=	M('CaseExtend')->field('case_id,remarks')->add(  $case_extend_data[$case_id]);
+                            if(false !== $result){
+                                // $number_of_add_extend 为新增到 case_extend 的数量
+                                $number_of_add_extend  = $number_of_add_extend  + 1;
+                                echo('case_source表第'.$i.'case表第'.$j.',新增到 case_extend<br>');
+                                dump($case_extend_data[$case_id]);
+                                //$this->success('修改成功', U('Case/view','case_id='.$case_id));
+                            }else{
+                                $this->error('校验后新增 CaseExtend 失败'.$i.'和'.$j);
+                            }
+                        }else{  // case_extend 表有对应于 case_id 的记录 
+                            $map_case_extend_for_update[$case_id]['case_extend_id'] =   $case_extend_list['case_extend_id'];
+                            
+                            if(empty( $case_extend_list['remarks'])){   //如果 case_extend 没有相应 remarks
+                                $case_extend_data[$case_id]['remarks'] = trim( $case_source_list[$i]['remarks']);
+                            }else{  //如果 case_extend 有相应 remarks
+                                $remarks_test  = strpos( trim( $$case_extend_list['remarks']),trim( $case_source_list[$i]['remarks']));
+                                if( FALSE == $remarks_test ){   //如果 Case_Extend 的数据未包含 Case_Source 的数据
+                                    $case_extend_data[$case_id]['remarks'] = trim( $case_extend_list['remarks']).'['.trim( $case_source_list[$i]['remarks']).']';
+                                }
+                            }                                                                       
+                             
+                             $result	=	M('CaseExtend')->where($map_case_extend_for_update[$case_id])->field('remarks')->save(  $case_extend_data[$case_id]);
+
+                             if(false !== $result){
+                                // $number_of_update_extend 为修改 case_extend 的数量
+                                $number_of_update_extend  = $number_of_update_extend  + 1;
+                                echo('case_source表第'.$i.'case表第'.$j.',更新到 case_extend<br>');
+                                dump($case_extend_data[$case_id]);
+                                //$this->success('修改成功', U('Case/view','case_id='.$case_id));
+                            }else{
+                                $this->error('校验后新增 CaseExtend 失败'.$i.'和'.$j);
+                            }
+                            
+                        }
+
+                    }
+                    
+                   
+                }  
+            }
         }
-      }
-     
-      
+         
+        echo('$number_of_new_case 为新case数量，等于'.$number_of_new_case.'<br>');
+        echo('$number_of_update_case 为更新的case数量，等于'.$number_of_update_case.'<br>');
+        echo('$number_of_diff_case 为信息有差异的case数量，等于'.$number_of_diff_case.'<br>');
+        echo('$number_of_add_extend 为新增到 case_extend 的数量，等于'.$number_of_add_extend.'<br>');
+        echo('$number_of_update_extend 为修改 case_extend 的数量，等于'.$number_of_update_extend.'<br>');
+       
     }
-    dump($n);
     
     
-		
-	}
-	
+    
+
 }
