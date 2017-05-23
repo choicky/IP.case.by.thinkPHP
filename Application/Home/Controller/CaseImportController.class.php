72301ba26f6f6a4cc78a68c81d617cde178dd773
Choicky ZHOU <zhoucaiqi@gmail.com>
2017-05-24 00:16:50 +0800

    修正 caseImport

--- Application/Home/Controller/CaseImportController.class.php
+++ Application/Home/Controller/CaseImportController.class.php
@@ -7,9 +7,19 @@ class CaseImportController extends Controller {
 	//默认显示
 	public function index(){
         //$this->display();
-        $map_case_extend['case_id'] = 527; 
-        $case_extend_list = M('CaseExtend')->where($map_case_extend)->find();
-        dump($case_extend_list);
+        $map['application_number'] = 19585094; 
+        $case_source_list = M('CaseSource')->where($map)->select();
+
+        dump($case_source_list);
+        
+        if(count($case_source_list)<1){
+            M('CaseOutput')->field('tentative_title,application_number,application_date,formal_title,issue_date,remarks')->add($case_source_list[0]);
+        
+        }
+        $case_output_list = M('CaseOutput')->where($map)->select();
+        
+        dump($case_output_list);
+
     }
 	
 	//导入
@@ -17,6 +27,8 @@ class CaseImportController extends Controller {
 		
         //读取 case_source 表
         $case_source_list  = M( 'CaseSource')->select();
+        
+        $case_source_count = count($case_source_list);
         
         /* 
         $i 为case_source 表的记录数量
@@ -28,12 +40,12 @@ class CaseImportController extends Controller {
         for( $i=0,$number_of_new_case=0,$number_of_update_case=0,$number_of_diff_case=0,$number_of_add_extend,$number_of_update_extend; $i<count( $case_source_list); $i++){
                    
             //用 application_number 作为匹配关键词
-            $map_case_for_find['application_number']  = trim( $case_source_list[$i]['application_number'] );
+            $map_case_for_find[$i]['application_number']  = trim( $case_source_list[$i]['application_number'] );
             
             //找出 case 表中的匹配记录
-            $case_list  = M( 'Case' )->where( $map_case_for_find)->select();
+            $case_list  = M( 'Case' )->where( $map_case_for_find[$i])->select();
             
-            if(!is_array($case_list)){ //如果 case 表没有匹配的记录，则原数据存入到 CaseOutput 表
+            if(count($case_list) < 1){ //如果 case 表没有匹配的记录，则原数据存入到 CaseOutput 表
                 $case_source_list[$i]['notfound'] = '白兔系统找到这个案子，但盈方管理系统没有找到；如该商标是有效的，要录入 admin.ipinfo.cn ';
                 $case_source_list[$i]['application_date'] = strtotime(trim( $case_source_list[$i]['application_date']));
                 $case_source_list[$i]['issue_date'] = strtotime(trim( $case_source_list[$i]['issue_date']));
@@ -43,6 +55,7 @@ class CaseImportController extends Controller {
                 if(false !== $result){
                     // $number_of_new_case 为新case数量
                     $number_of_new_case  = $number_of_new_case  + 1;
+                    echo('这是在 count($case_list) <1 里面'.count($case_list).'<br>');
                     echo('case_source表第'.$i.'case表第'.$j.',新增到 case_output<br>');
                     dump($case_source_list[$i]);
                     
@@ -66,6 +79,8 @@ class CaseImportController extends Controller {
                                 $case_data[$case_id]['formal_title_test'] = '盈方系统原来登记的分类号是：'.trim($case_data[$case_id]['formal_title']).'，'.'已将白兔的分类号：'.$case_source_list[$i]['formal_title'].'，加到中括号里';
                                 $case_data[$case_id]['formal_title'] = trim( $case_data[$case_id]['formal_title']).'['.trim( $case_source_list[$i]['formal_title']).']';
                             }
+                        }else{  ////如果 case 没有相应数据，或者两者数据相同
+                            $case_data[$case_id]['formal_title'] = trim( $case_source_list[$i]['formal_title']);
                         }
                     }
                       
@@ -77,6 +92,8 @@ class CaseImportController extends Controller {
                                 $case_data[$case_id]['tentative_title_test'] = '盈方系统原来登记的商标名称是：'.trim( $case_data[$case_id]['tentative_title']).'，'.'已将白兔的商标名称：'.trim( $case_source_list[$i]['tentative_title']).'加到中括号里';
                                 $case_data[$case_id]['tentative_title'] = trim( $case_data[$case_id]['tentative_title']).'['.trim( $case_source_list[$i]['tentative_title']).']'; 
                             }
+                        }else{  ////如果 case 没有相应数据，或者两者数据相同
+                            $case_data[$case_id]['tentative_title'] = trim( $case_source_list[$i]['tentative_title']);
                         }
                     }
                       
@@ -88,6 +105,8 @@ class CaseImportController extends Controller {
                                 $case_data[$case_id]['application_date_test'] = '盈方系统原来登记的申请日是：'.date("Y-m-d",trim( $case_data[$case_id]['application_date'])).'，'.'已更新为白兔的申请日：'.trim( $case_source_list[$i]['application_date']).'，请核对';
                                 $case_data[$case_id]['application_date'] = strtotime(trim( $case_source_list[$i]['application_date'])); 
                             }
+                        }else{  ////如果 case 没有相应数据，或者两者数据相同
+                            $case_data[$case_id]['application_date'] = strtotime(trim( $case_source_list[$i]['application_date']));
                         }
                     }
                       
@@ -99,6 +118,8 @@ class CaseImportController extends Controller {
                                 $case_data[$case_id]['issue_date_test'] = '盈方系统原来登记的发证日是：'.date("Y-m-d",trim( $case_data[$case_id]['issue_date'])).'，'.'已更新为白兔的发证日：'.trim( $case_source_list[$i]['issue_date']).'，请核对';
                                 $case_data[$case_id]['issue_date'] = strtotime(trim( $case_source_list[$i]['issue_date']));
                             }
+                        }else{  ////如果 case 没有相应数据，或者两者数据相同
+                            $case_data[$case_id]['issue_date'] = strtotime(trim( $case_source_list[$i]['issue_date']));
                         }
                     }
                       
@@ -141,7 +162,7 @@ class CaseImportController extends Controller {
                         $map_case_extend_for_find['case_id'] = $case_list[$j]['case_id'];
                         $case_extend_list = M('CaseExtend')->where( $map_case_extend_for_find)->find();
                         
-                        if(!is_array($case_extend_list)){   // case_extend 表没有对应于 case_id 的记录
+                        if(count($case_extend_list) < 1 ){   // case_extend 表没有对应于 case_id 的记录
                             $case_extend_data[$case_id]['case_id'] = $case_list[$j]['case_id'];
                             $case_extend_data[$case_id]['remarks'] = trim( $case_source_list[$i]['remarks']);
                             $result	=	M('CaseExtend')->field('case_id,remarks')->add(  $case_extend_data[$case_id]);
